e0f7f2fbd680bd7af44e399d5c1f63fb7917215e
Peter McMaster <39925732+mcmasterml@users.noreply.github.com>
2024-11-27 12:54:28 -0800

    Reorg css into one file per section

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 // navbar
 // toggle visibility
-const toggleButton = document.querySelector('.toggle-button');
-const navbarLinks = document.querySelector('.navbar-links');
+// const toggleButton = document.querySelector('.toggle-button');
+// const navbarLinks = document.querySelector('.navbar-links');
 
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active');
+// toggleButton.addEventListener('click', () => {
+//     navbarLinks.classList.toggle('active');
+// });
+
+
+document.querySelector('.toggle-button').addEventListener('click', function() {
+    document.querySelector('.navbar-links').classList.toggle('active');
 });
